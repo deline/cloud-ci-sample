feeff50e197d67d63102b5c635024b9ae0e7bec2
Deline Neo <delineneo@gmail.com>
2017-03-22 07:07:15 +1100

    Add initial fastlane lane, and unit tests

--- cloud-ci-sampleTests/cloud_ci_sampleTests.swift
+++ cloud-ci-sampleTests/cloud_ci_sampleTests.swift
@@ -13,24 +13,18 @@ class cloud_ci_sampleTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPassingTest() {
+        XCTAssert(1 == 1)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testFailingTest() {
+        XCTAssert(1 == 2, "1 does not equal 2")
     }
     
 }
